174e078a71c9f179cd9955bcd422a4a8de4091ca
runaq <pan4amka@gmail.com>
2018-05-16 00:56:10 +0300

    re-models, post-list

--- loc/models.py
+++ loc/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import RegexValidator
 
 class Post(models.Model):
     id = models.AutoField( primary_key=True)
@@ -9,7 +10,6 @@ class Post(models.Model):
     lat=models.DecimalField(max_digits=10, decimal_places=8, blank=False, null=False, verbose_name='Широта')
     lot=models.DecimalField(max_digits=11, decimal_places=8, blank=False, null=False, verbose_name='Долгота')
     phone_number = PhoneNumberField()
-
-	
+     	
     def __str__(self):
-        return self.id
+        return self.first_name
